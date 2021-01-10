de907f7320f1f1f6536c50bf7dfda3f813aa5620
kubilay <isen.kubilay@gmail.com>
2021-01-10 22:40:58 +0300

    added README.md

---
+++ recaman.py
@@ -0,0 +1,14 @@
+from itertools import count
+
+def recaman():
+    """Generate Recaman's sequence
+    """
+    seen = set()
+    a = 0
+    for n in count(1):
+        yield a 
+        seen.add(a)
+        c = a - n 
+        if c < 0 or c in seen:
+            c = a + n
+        a = c
